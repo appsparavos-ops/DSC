919307d7afe3bc0acd836871cbd7903cb700769d
appsparavos-ops <appsparavos@gmail.com>
2026-02-04 13:04:50 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANWXQvhHpF0LCYjz4AXi3MkcP798PqRfA",
+    authDomain: "dsc24-aa5a1.firebaseapp.com",
+    databaseURL: "https://dsc24-aa5a1-default-rtdb.firebaseio.com",
+    projectId: "dsc24-aa5a1",
+    storageBucket: "dsc24-aa5a1.appspot.com",
+    messagingSenderId: "798100493177",
+    appId: "1:798100493177:web:8e2ae324f8b5cb893a55a8"
+};
