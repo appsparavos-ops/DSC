0ab584ac38baccb50e9ac5ca16c5733fbcec1929
appsparavos-ops <appsparavos@gmail.com>
2026-03-16 14:33:13 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const GITHUB_CONFIG = {
-    // Codificado en Base64 para evitar que el escáner de GitHub lo detecte y lo elimine
-    token: atob("Z2hwXzd6cEFjTWZZVzhEODBsUHRuOHF0ZE10SW5KcG1RUzJTZGkxSw=="),
-    owner: "appsparavos-ops",
-    repo: "DSC",
-    path: "cartas"
-};
